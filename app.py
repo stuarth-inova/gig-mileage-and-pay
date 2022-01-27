9d51f55308e6757207e4c18607e3299850fc0540
Stuart Holme <stuart.holme@willowtreeapps.com>
2022-01-26 22:37:42 -0500

    Print gig list (#9)
    * first pass at list output
    
    * got alpha order venue list output

--- app.py
+++ app.py
@@ -50,6 +50,13 @@ def hello(name=None):
     return render_template('hello.html', name=name)
 
 
+@app.route('/venue-list')
+def venue_list():
+    raw_venues = Venue.query.order_by(asc(Venue.venue)).all()
+    print('venue list is type: {}'.format(type(raw_venues)))
+    return render_template('venue_details.html', venue_dict=raw_venues)
+
+
 @app.route('/gigs')
 @app.route('/gigs/')
 def gigs():
@@ -59,7 +66,7 @@ def gigs():
 @app.route('/gigs/<gig_year>')
 def gig_details(gig_year):
     try:
-        return render_template('gig_details.html', data_file=gig_year, gig_dict=db_printable_gig_list(gig_year))
+        return render_template('gig_details.html', gig_year=gig_year, gig_dict=db_printable_gig_list(gig_year))
     except ValueError as val_err:
         return render_template('error.html', exception=val_err)
 
