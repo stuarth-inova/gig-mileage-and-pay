a71098f51cb034d583a2b43d2ae9660f91949afb
stuarth-inova <stuart.holme@willowtreeapps.com>
2023-02-11 01:06:11 -0500

    catching duplicates, still need form validation

--- app.py
+++ app.py
@@ -90,13 +90,14 @@ def gig_details(gig_year):
 
 
 @app.route('/gigs/submit')
-def enter_new_gig():
+@app.route('/gigs/submit/<error_message>')
+def enter_new_gig(error_message=None):
     """
     Renders a form for entry of gig data
     :return:
     """
     venues = Venue.query.order_by(asc(Venue.venue)).all()
-    return render_template('submit_new_gig.html', venues_dict=venues)
+    return render_template('submit_new_gig.html', venues_dict=venues, error_message=error_message)
 
 
 @app.route('/gigs/new_gig_data', methods=['POST', 'GET'])
@@ -113,6 +114,28 @@ def submit_new_gig():
         pay = request.form.get('pay')
         trip_origin = request.form.get('trip_origin')
         comment = request.form.get('comment')
+
+        return_gig_submit = False
+        error_message = ''
+        if pay is None:
+            return_gig_submit = True
+            error_message = 'Pay amount not entered /r'
+
+        if band is None:
+            return_gig_submit = True
+            error_message = error_message + 'No band in submission /r'
+
+        check_duplicate = [(gig.gig_date, gig.venue, gig.band) for gig in db.session.query(
+            Gig.gig_date, Gig.venue, Gig.band)
+            .filter(Gig.gig_date == date.fromisoformat(gig_date)).filter(Gig.venue == venue).filter(Gig.band == band)]
+        if len(check_duplicate) > 0:
+            return_gig_submit = True
+            error_message = error_message + 'Duplicate Gig Entry!'
+
+        if return_gig_submit:
+            print('SHOULD REDIRECT w/ ERROR')
+            return redirect(url_for('enter_new_gig', venues_dict=Venue.query.order_by(asc(Venue.venue)).all(),
+                                    error_message=error_message))
 
         existing_venue = Venue.query.filter_by(venue=venue.lower()).first()
         if existing_venue is None:
